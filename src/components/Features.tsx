4530ee17890377274018b75c4bd19686f914d57a
Julian Krochmal <juliankrochmal@gmail.com>
2021-11-20 18:38:43 -0300

    deleting functional component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, ReactElement, useState } from 'react';
+import { ReactElement, useState } from 'react';
 import { FaGlobe, FaTrophy, FaKey, FaLock, FaChartLine, FaMap } from 'react-icons/fa';
 
 interface FeaturesT {
@@ -8,15 +8,15 @@ interface FeaturesT {
     description: string
 }
 
-const Features: FunctionComponent = () => {
+const Features = () => {
 
     const [features] = useState<FeaturesT[]>([
-        { id: 1, icon: <FaGlobe className= 'feature__icon'/>, title: "World's best luxury homes", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
-        { id: 2, icon: <FaTrophy className= 'feature__icon'/>, title: "Only the best properties", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
-        { id: 3, icon: <FaMap className= 'feature__icon'/>, title: "All homes in top location", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
-        { id: 4, icon: <FaKey className= 'feature__icon'/>, title: "New home in one week", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
-        { id: 5, icon: <FaChartLine className= 'feature__icon'/>, title: "Top 1% realtors", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
-        { id: 6, icon: <FaLock className= 'feature__icon'/>, title: "Secure payments on nexter", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." }
+        { id: 1, icon: <FaGlobe className='feature__icon' />, title: "World's best luxury homes", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
+        { id: 2, icon: <FaTrophy className='feature__icon' />, title: "Only the best properties", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
+        { id: 3, icon: <FaMap className='feature__icon' />, title: "All homes in top location", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
+        { id: 4, icon: <FaKey className='feature__icon' />, title: "New home in one week", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
+        { id: 5, icon: <FaChartLine className='feature__icon' />, title: "Top 1% realtors", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." },
+        { id: 6, icon: <FaLock className='feature__icon' />, title: "Secure payments on nexter", description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia error, aliquid quibusdam commodi itaque et veniam quasi cupiditate repellendus." }
     ])
 
     return (
